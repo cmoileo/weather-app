64edd4b110abfb59a6e67259270db00e04cc5e66
Fezard Léo <leo.fezard33@gmail.com>
2023-02-21 12:54:57 +0100

    [ADD] search bar

--- src/Components/RightMetrics/CityHeader/CityHeader.jsx
+++ src/Components/RightMetrics/CityHeader/CityHeader.jsx
@@ -1,9 +1,23 @@
+import { useRef } from 'react'
 import PinIcon from '../../../Assets/pin-icon.svg'
-import WenIcon from '../../../Assets/wen-icon.svg'
 import useAPIContext from '../../../Contexts/APIContext'
 
 export default function CityHeader() {
-  const {city} = useAPIContext()
+  const {city, setCity, getApiData} = useAPIContext()
+
+  const searchBarRef = useRef(null)
+  const inputRef = useRef(null)
+
+  const handleMouseEnter = () => {
+    inputRef.current.style.width = "350px"
+  }
+  const handleMouseLeave = () => {
+    inputRef.current.style.width = "20px"
+  }
+  const handleClick = () => {
+    setCity(inputRef.current.value)
+    getApiData()
+  }
 
   return (
     <div className="city-header-container">
@@ -11,7 +25,10 @@ export default function CityHeader() {
             <img src={PinIcon} alt="" />
             <h3>{city}</h3>
         </div>
-        <img src={WenIcon} alt="" />
+        <div class="city-search-bar" ref={searchBarRef} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+          <input placeholder='Search...' class='js-search' type="text" ref={inputRef}/>
+          <i class="fa fa-search" onClick={handleClick}></i>
+        </div>
     </div>
   )
 }
--- src/Contexts/APIContext.jsx
+++ src/Contexts/APIContext.jsx
@@ -7,34 +7,34 @@ export const APIContextProvider = ({children }) => {
     const [city, setCity] = useState('Bordeaux, France');
 
     const [tempMetrics, setTempMetrics] = useState("cel")
+
+    async function getApiData() {
+      let coordinates = await fetch(
+        "http://api.openweathermap.org/geo/1.0/direct?q=" +
+        city +
+        "&limit=5&appid=592f1fe6fbbdf00e5d4ac9bfee6bf0ae"
+      ).then((coordinates) => coordinates.json());
+      let lat = coordinates[0].lat;
+      let lon = coordinates[0].lon;
+      const response = await fetch (
+        "https://api.openweathermap.org/data/3.0/onecall?lat=" +
+        lat +
+        "&lon=" +
+        lon +
+        "&appid=d057e468d4ec84cf017e94a0d6911d7b"
+      );
+      const jsonData = await response.json();
+      setApiData(jsonData);
+    }
   
     useEffect(() => {
-      async function getApiData() {
-        let coordinates = await fetch(
-          "http://api.openweathermap.org/geo/1.0/direct?q=" +
-          city +
-          "&limit=5&appid=592f1fe6fbbdf00e5d4ac9bfee6bf0ae"
-        ).then((coordinates) => coordinates.json());
-        let lat = coordinates[0].lat;
-        let lon = coordinates[0].lon;
-        const response = await fetch (
-          "https://api.openweathermap.org/data/3.0/onecall?lat=" +
-          lat +
-          "&lon=" +
-          lon +
-          "&appid=d057e468d4ec84cf017e94a0d6911d7b"
-        );
-        const jsonData = await response.json();
-        setApiData(jsonData);
-      }
-
       getApiData()
   
     }, [city]);
 
 
     return (
-        <APIContext.Provider value={{ apiData, city, tempMetrics, setTempMetrics }}>
+        <APIContext.Provider value={{ apiData, city, setCity, tempMetrics, setTempMetrics, getApiData }}>
             { children }
         </APIContext.Provider>
     );
